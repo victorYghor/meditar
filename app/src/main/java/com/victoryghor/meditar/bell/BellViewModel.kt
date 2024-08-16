31b33362e2b5f6a7fda8fe5a9a986e41c4b5e5f9
vitorYghor <devictoryghor@gmail.com>
2024-08-16 17:10:45 -0300

    feature: add the bell viewModel to play music

--- app/src/main/java/com/victoryghor/meditar/bell/BellViewModel.kt
+++ app/src/main/java/com/victoryghor/meditar/bell/BellViewModel.kt
@@ -1,7 +1,21 @@
 package com.victoryghor.meditar.bell
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class BellViewModel: ViewModel() {
-
+class BellViewModel(): ViewModel() {
+    fun startRingBell(goToSelectTimer: () -> Unit) {
+        viewModelScope.launch {
+            // tocar o sino
+            delay(5_000L)
+            withContext(Dispatchers.Main) {
+                goToSelectTimer()
+            }
+        }
+    }
 }
